5b56d48f804783bdad87ce664aeeba5ebca55af8
Alex <alex98hun@gmail.com>
2023-06-19 19:51:27 +0200

    Create dotfiles.sh

---
+++ dotfiles.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#
+# dotfiles.sh
+#
+
+sym() {
+    for file in .*; do
+        case $file in
+            .) ;;
+            ..) ;;
+            *) [ -f "$file" ] && ln -sfv "$PWD/$file" "$HOME/$file" ;;
+        esac
+    done
+
+#   [ -d "$HOME/.mpd/playlists" ] || mkdir -p "$HOME/.mpd/playlists"
+
+    printf '\nDone.\n'
+}
+
+ask() {
+    printf '%s\n' "Symlink dotfiles?: Press Enter to continue or Ctrl+C to abort."
+    read -r _
+}
+
+main() {
+    ask
+    sym
+}
+
+main
